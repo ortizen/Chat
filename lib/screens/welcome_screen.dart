9aa3f201f461c5738db09f1d9a8efacb3dc2c240
Enrique Ortiz <oice6703@gmail.com>
2019-09-22 10:48:46 -0500

    Added animations

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,7 +5,29 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    controller.forward();
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
+    controller.addListener(() {
+      // print(animation.value);
+      setState(() {});
+    });
+    animation.addStatusListener((status) {
+      print(status);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +44,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: controller.value * 100,
                   ),
                 ),
                 Text(
